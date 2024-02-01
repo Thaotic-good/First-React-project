dd424446683db6a315eb903e923a91faa7150185
thaotic-good <fu3martina@gmail.com>
2024-02-01 16:28:26 +0100

    ToDoApp component finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import MyButton  from './MyButton.jsx'
+import MyButton from './MyButton.jsx'
 import Timer from './Timer'
 import ColorChanger from "./ColorChanger.jsx";
+import ToDoApp from "./ToDoApp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyButton />
-        <Timer/>
-        <ColorChanger/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <ToDoApp/>
+                <MyButton />
+                <Timer/>
+                <ColorChanger/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
