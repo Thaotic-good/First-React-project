51cb57d8cd9cca9b56f0f48922fb6bf3914448fc
thaotic-good <fu3martina@gmail.com>
2024-03-03 14:30:45 +0100

    Adding image sources to RedPillBluePill

--- src/AlgorithMemoryTest.jsx
+++ src/AlgorithMemoryTest.jsx
@@ -1,20 +1,42 @@
 import React, {useState} from "react";
+import expectationsImg from './images/expectations.webp';
+import realityImg from './images/reality.webp'
+/*! path to an image needs to be assigned to a variable :D*/
 /*BLUE PILL RED PILL CHOICE
 * 1. first blue button will show text 'Expectation: I'm rich!!! ' and picture generated from DallE
 * 2. second red button will show text 'Reality: Am I retarded?' and a generated picture
 * __________________________________________________
 * 1. useState will update a message and image shown
-* 2. I could add a mirror frame, and mirror picture, but images would have to be the same size */
+* 2. I need for each button to change the image as well as the message
+* 3. I could add a mirror frame, and mirror picture, but images would have to be the same size */
 
 function RedPillBluePill(){
     const [message, setMessage]= useState("")
+    const [illustration, setIllustration ]= useState("mirror")
+    const [alt, setAlt] = useState('')
+
+    const blueButtonHandler = ()=>{
+        setMessage("This is easy and I'm rich!!!")
+        setIllustration(expectationsImg)
+        setAlt("A small developer lying on a laptop on a beach surrounded with gold coins and a cocktail")
+    }
+
+    const redButtonHandler = () => {
+        setMessage("This is hard! Am I retarded?")
+        setIllustration(realityImg)
+        setAlt("A developer in a hoodie in front of a computer holding his head in a dark-lit room surrounded by heaps of empty coffe cups")
+    }
 
     return (
         <>
-            <img src={}/>
+            <img style={{width: '200px'}} src={illustration} alt={alt}/>
             <div className="message-text">{message}</div>
-            <button onClick={()=>setMessage("Expectation: This is easy and I'm rich!!!")}>Blue pill</button>
-            <button onClick={()=> setMessage("Reality: This is hard! Am I retarded?")}>Red pill</button>
+            <div>
+            <button style={{backgroundColor: 'blue', borderRadius:'70px'}} onClick={blueButtonHandler}>Blue pill</button>
+            <button style={{backgroundColor: 'red', borderRadius:'70px'}} onClick={redButtonHandler}>Red pill</button>
+            </div>
         </>
     )
 }
+
+export default RedPillBluePill
--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Timer from './Timer'
 import NippleColorChanger from "./NippleColorChanger.jsx";
 import SignUpForm from "./SignUpForm";
 import DarkModeToggle from "./DarkModeToggle.jsx";
+import RedPillBluePill from "./AlgorithMemoryTest";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
+                <RedPillBluePill/>
                 <DarkModeToggle/>
                 <SignUpForm/>
                 <Timer/>
